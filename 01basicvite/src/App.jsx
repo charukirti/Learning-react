1df6bed4c2395867bf2326c7dd331d97135480ac
charukirti <charukirticc112002@gmail.com>
2024-02-22 16:40:39 +0530

    Installed react by both classic way and using vite. And learned to make my own component

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,18 @@
+import Youtube from './youtube'
+import RenderName from './charukirti'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Vite react app</h1>
+     <h1>Vite react app</h1>
+    <Youtube />
+    <RenderName />
+    </>
+   
+  )
+}
+
+export default App
---
+++ 01basicvite/src/charukirti.jsx
@@ -0,0 +1,7 @@
+function RenderName(){
+    return (
+        <h1>Hello👋, this is Charukirti here. See you while learning react after 25th of FEB</h1>
+    )
+}
+
+export default RenderName
