8226c04836b187a310fce445b43a57d9ecc6c6f2
charukirti <charukirticc112002@gmail.com>
2024-02-24 12:24:13 +0530

    Learnt about how to create own react library and jsx

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,13 +1,14 @@
 import Youtube from './youtube'
 import RenderName from './charukirti'
 
+
 function App() {
- 
+  const username = 'charukirti'
 
   return (
     <>
-     <h1>Vite react app</h1>
-     <h1>Vite react app</h1>
+     <h1>Vite react app {2 + 2}</h1>
+     <h1>Vite react app {username}</h1>
     <Youtube />
     <RenderName />
     </>
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,9 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Learning and creating react lib
+
+const reactElem = {
+  type: 'a',
+  props: {
+    href: 'https://google.com',
+    target: '_blank',
+  },
+  children: 'click me to visit google',
+}
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom React app</h1>
+    </div>
+  )
+}
+
+const AnotherElem = (
+  <a href="https://google.com" target="_blank">
+    Head towards google
+  </a>
+)
+
+const actualReactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'Click to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+  <App />
+
+  //    <MyApp/>
+
+//   actualReactElement
 )
