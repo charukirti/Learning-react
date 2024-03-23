0926dd58a08a9685f57a4fb7705b4c64b6808473
charukirti <charukirticc112002@gmail.com>
2024-03-23 17:56:10 +0530

    initial commit

---
+++ practice-projects/06Modal/src/App.jsx
@@ -0,0 +1,14 @@
+import Popup from './components/Popup'
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ practice-projects/06Modal/src/components/Popup.jsx
@@ -0,0 +1,9 @@
+function Popup() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Popup
