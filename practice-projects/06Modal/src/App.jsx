69350ee0e4ceccfefad911f0506340805d3ef0d6
charukirti <charukirticc112002@gmail.com>
2024-03-23 18:46:06 +0530

    created simple modal

--- practice-projects/06Modal/src/App.jsx
+++ practice-projects/06Modal/src/App.jsx
@@ -1,14 +1,20 @@
-import Popup from './components/Popup'
-import { useState } from 'react'
+import Popup from "./components/Popup";
+import { useState } from "react";
 
 function App() {
-  
+  const [openModal, setOpenModal] = useState(false);
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      <button className="btn-open" 
+      onClick={() => setOpenModal(true)}>
+        Open modal
+      </button>
+
+      {openModal && <Popup closeModal={setOpenModal}/>}
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
--- practice-projects/06Modal/src/components/Popup.jsx
+++ practice-projects/06Modal/src/components/Popup.jsx
@@ -1,9 +1,24 @@
-function Popup() {
-    return (
-        <div>
-            
+import './Popup.css'
+
+function Popup({closeModal}) {
+  return (
+    <div className="bg-overlay">
+      {/* modal content */}
+      <div className="modal-container">
+        <button className="btn-close" onClick={() => closeModal(false)}>X</button>
+        <div className="title">
+          <h1>Are you sure you want to continue</h1>
         </div>
-    )
+        <div className="body">
+          Lorem, ipsum dolor sit amet consectetur adipisicing.
+        </div>
+        <div className="footer">
+          <button onClick={() => closeModal(false)} className='btn-cancle'>Cancle</button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Popup
+export default Popup;
