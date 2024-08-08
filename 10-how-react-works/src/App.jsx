3b713ced86869d9cc7221cf7fb1f82b8dfcb65d9
charukirti <charukirticc112002@gmail.com>
2024-08-08 18:44:29 +0530

    Learnt about state batching

--- 10-how-react-works/src/App.jsx
+++ 10-how-react-works/src/App.jsx
@@ -26,8 +26,8 @@ export default function App() {
   );
 }
 
-console.log(<DifferentContent/>)
-console.log(DifferentContent())
+console.log(<DifferentContent />);
+console.log(DifferentContent());
 
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
@@ -42,12 +42,13 @@ function Tabbed({ content }) {
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} key={content.at(activeTab).summary}/>
+        <TabContent
+          item={content.at(activeTab)}
+          key={content.at(activeTab).summary}
+        />
       ) : (
         <DifferentContent />
       )}
-
-      
     </div>
   );
 }
@@ -67,8 +68,26 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
+
+  function handleTrippleInc() {
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+    setLikes((likes) => likes + 1);
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000)
   }
 
   return (
@@ -84,13 +103,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button onClick={handleInc}>+++</button>
+          <button onClick={handleTrippleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
