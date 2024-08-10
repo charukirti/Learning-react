796b247cb924c9aba4dcba4db71d4178c3a987aa
charukirti <charukirticc112002@gmail.com>
2024-08-10 19:22:12 +0530

    reset

--- 09-usePopcorn/src/main.jsx
+++ 09-usePopcorn/src/main.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.jsx'
-// import './index.css'
-import StarRating from "./StarRating";
+import App from './App.jsx'
+import './index.css'
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0)
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating = {setMovieRating}/>
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0)
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating = {setMovieRating}/>
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} className="test" defaultRating={3} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
