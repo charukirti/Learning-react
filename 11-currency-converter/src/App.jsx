733b1e720b6fb6712772e13511985ad465c9f7aa
charukirti <charukirticc112002@gmail.com>
2024-08-15 23:02:39 +0530

    starter

--- 11-currency-converter/src/App.jsx
+++ 11-currency-converter/src/App.jsx
@@ -1,8 +1,22 @@
-export default function App (){
+export default function App() {
   return (
     <>
-      
+      <div>
+        <input type="text" />
+        <select>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <select>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <p>OUTPUT</p>
+      </div>
     </>
-  )
+  );
 }
-
