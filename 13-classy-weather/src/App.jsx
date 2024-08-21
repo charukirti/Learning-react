84c683ab2a989452ecb131d3a4b5fc6411b9e784
charukirti <charukirticc112002@gmail.com>
2024-08-21 22:56:44 +0530

    started learning about class components

--- 13-classy-weather/src/App.jsx
+++ 13-classy-weather/src/App.jsx
@@ -1,2 +1,26 @@
+import React from "react";
+// import './App.css'
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { count: 5 };
+  }
+
+  handleDecrement(){
+    console.log(this)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button>+</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
