f09ba72ad86e5886a2a37002b7da39e6ad0c98f9
charukirti <charukirticc112002@gmail.com>
2024-08-27 21:39:49 +0530

    'had a basic overview of useReducer hook'

--- 14-react-quiz/src/DateCounter.jsx
+++ 14-react-quiz/src/DateCounter.jsx
@@ -1,15 +1,17 @@
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
-  console.log(state, action)
+  console.log(state, action);
 
-  return state + action;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "setCount") return action.payload;
 }
 
 export default function DateCounter() {
   // const [count, setCount] = useState(0);
 
-  const [count, dispatch] = useReducer(reducer, 0)
+  const [count, dispatch] = useReducer(reducer, 0);
 
   const [step, setStep] = useState(1);
 
@@ -18,20 +20,20 @@ export default function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1)
-    
+    dispatch({ type: "dec"});
+
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-      dispatch(1)
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    dispatch(Number(e.target.value))
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
