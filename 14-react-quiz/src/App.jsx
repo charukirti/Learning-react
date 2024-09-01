85e997351b219e280c5cc043c48866d1095efb75
charukirti <charukirticc112002@gmail.com>
2024-09-01 23:31:28 +0530

    started working on quiz app

--- 14-react-quiz/src/App.jsx
+++ 14-react-quiz/src/App.jsx
@@ -1,11 +1,59 @@
-import './App.css'
-import DateCounter from './DateCounter';
+import { useEffect, useReducer } from "react";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+// import DateCounter from './DateCounter';
 
-export default function App (){
-  return (
-    <>
-      <DateCounter/>
-    </>
-  )
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
+export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length
+
+  useEffect(function () {
+    fetch("http://localhost:5174/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />} {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions = {numQuestions}/>}
+      </Main>
+    </div>
+  );
+}
