03426089895f17892d03e3f7446defbf1bd38055
charukirti <charukirticc112002@gmail.com>
2024-09-03 21:55:23 +0530

    added progressbar component

---
+++ 14-react-quiz/src/components/Progress.jsx
@@ -0,0 +1,20 @@
+export default function Progress({
+  index,
+  numQuestion,
+  points,
+  maxPossiblePonts,
+  answer,
+}) {
+  return (
+    <header className="progress">
+      <progress max={numQuestion} value={index + Number(answer !== null)} />
+      <p>
+        Question: <strong>{index + 1}</strong> / {numQuestion}
+      </p>
+
+      <p>
+        <strong>{points}</strong>/ {maxPossiblePonts}
+      </p>
+    </header>
+  );
+}
