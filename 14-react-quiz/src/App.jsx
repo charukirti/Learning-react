8aecb6e777a033be86dc0690463859e2da2eb9da
charukirti <charukirticc112002@gmail.com>
2024-09-03 21:56:05 +0530

    Added logic for selecting answer and progress

--- 14-react-quiz/src/App.jsx
+++ 14-react-quiz/src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useReducer } from "react";
 import "./App.css";
-import Header from "./Header";
-import Main from "./Main";
-import Loader from "./Loader";
-import Error from "./Error";
-import StartScreen from "./StartScreen";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 // import DateCounter from './DateCounter';
 
 const initialState = {
@@ -12,6 +15,9 @@ const initialState = {
 
   // 'loading', 'error', 'ready'
   status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -29,15 +35,43 @@ function reducer(state, action) {
         status: "error",
       };
 
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
     default:
       throw new Error("Unknown action");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
-  const numQuestions = questions.length
+  const numQuestions = questions.length;
+  const maxPossiblePonts = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:5174/questions")
@@ -52,7 +86,27 @@ export default function App() {
 
       <Main>
         {status === "loading" && <Loader />} {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions = {numQuestions}/>}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestion={numQuestions}
+              points={points}
+              maxPossiblePonts={maxPossiblePonts}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
