50957212b2bd6c54731a93d619a2fd8bcd7dffee
charukirti <charukirticc112002@gmail.com>
2024-09-09 19:26:36 +0530

    Added routes and linked pages

--- 16-worldwise/src/App.jsx
+++ 16-worldwise/src/App.jsx
@@ -1,9 +1,22 @@
-export default function App (){
-  const x = 10;
-  return (
-    <>
-      <h1>World Wise!</h1>
-    </>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ 16-worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import AppNav from "../components/AppNav";
+
+export default function AppLayout (){
+    return (
+        <>  
+            <AppNav/>
+            <h1>App Layout</h1>
+        </>
+    )
+}
+
