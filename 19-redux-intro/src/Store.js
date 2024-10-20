85d354087cca75e42fd6db48e7de1f527f0e7b58
charukirti <charukirticc112002@gmail.com>
2024-10-20 18:49:46 +0530

    Learnt about modern redux file structure and implemented it

--- 19-redux-intro/src/Store.js
+++ 19-redux-intro/src/Store.js
@@ -1,136 +1,22 @@
 import { combineReducers, createStore } from "redux";
 
+// importing reducers
 
-// created reducer 
-
-const initialAccountState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: '',
-};
-
-const initialCustomerState = {
-    fullName: '',
-    nationalID: '',
-    createdAt: ''
-};
-
-function accountReducer(state = initialAccountState, action) {
-    switch (action.type) {
-        case 'account/deposit':
-            return { ...state, balance: state.balance + action.payload };
-
-        case 'account/withdraw':
-            return { ...state, balance: state.balance - action.payload };
-
-        case 'account/requestLoan':
-            if (state.loan > 0) return 0;
-            // LATER
-            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount };
-        case 'account/payLoan':
-            return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan };
-
-        default:
-            return state;
-    }
-}
-
-
-function customerReducer(state = initialCustomerState, action) {
-    switch (action.type) {
-        case 'customer/createCustomer':
-            return { ...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt };
-
-        case 'customer/updateName':
-            return { ...state, fullName: action.payload };
-        default:
-            return state;
-    }
-}
-
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
 // combining all the reducers
 
 const rootReducer = combineReducers({
     account: accountReducer,
     customer: customerReducer,
-})
+});
 
-// creating store
 
 const store = createStore(rootReducer);
 
-// store.dispatch({ type: 'account/deposit', payload: 1200 });
-// store.dispatch({ type: 'account/withdraw', payload: 120 });
-// console.log(store.getState());
 
-// store.dispatch({ type: 'account/requestLoan', payload: { amount: 1000, purpose: 'Buy a car' } });
-// console.log(store.getState());
+// exporting store for integrating in react
 
-// store.dispatch({ type: 'account/payLoan' });
-// console.log(store.getState())
+export default store
 
-
-// practicing creating store
-
-// store.dispatch({ type: 'account/deposit', payload: 2000 });
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/withdraw', payload: 80 });
-// console.log(store.getState());
-
-// store.dispatch();
-// console.log(store.getState());
-
-// store.dispatch({ type: 'account/payLoan' });
-// console.log(store.getState())
-
-
-// Action creators for each actions
-
-function deposit(amount) {
-    return { type: 'account/deposit', payload: amount };
-}
-
-function withdraw(amount) {
-    return { type: 'account/withdraw', payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-    return { type: 'account/requestLoan', payload: { amount, purpose } };
-}
-
-function payLoan() {
-    return { type: 'account/payLoan' };
-}
-
-
-store.dispatch(deposit(5000));
-console.log(store.getState());
-
-store.dispatch(withdraw(3000));
-console.log(store.getState());
-
-store.dispatch(requestLoan(3000, 'To buy new course'));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-store.dispatch(deposit(3000));
-console.log(store.getState());
-
-
-function createCustomer(fullName, nationalID) {
-    return { type: 'customer/createCustomer', payload: { fullName, nationalID, createdAt: new Date().toISOString() } };
-}
-
-function updateName(fullName) {
-    return { type: 'customer/updateName', payload: fullName };
-}
-
-
-// using customer actions
-
-store.dispatch(createCustomer('Albert', '21AB4'))
-console.log(store.getState())
--- 19-redux-intro/src/main.jsx
+++ 19-redux-intro/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './Store.js'
+import store from './Store.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
