8b86fc0bba6a05dfe74ee2f4e0a4c6491ec6e875
charukirti <charukirticc112002@gmail.com>
2024-11-19 12:34:34 +0530

    created loader,  and learned about handling errors with  error elements

--- fast-react-pizza/src/features/menu/Menu.jsx
+++ fast-react-pizza/src/features/menu/Menu.jsx
@@ -3,7 +3,8 @@ import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from './MenuItem'
 function Menu() {
  const menu =  useLoaderData()
- console.log(menu)
+ 
+
   return <ul>
     {menu.map(pizza => <MenuItem key={pizza.id} pizza={pizza}/>)}
   </ul>;
