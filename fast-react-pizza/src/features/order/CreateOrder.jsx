0afd2af71f9640660aad64217cf7d60d42c4686a
charukirti <charukirticc112002@gmail.com>
2024-11-21 11:47:09 +0530

    styled order form inputs and learnt about reusing tailwind styles with @apply rult

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import {createOrder} from '../../services/apiRestaurant/'
+import { createOrder } from "../../services/apiRestaurant/";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -33,9 +33,8 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-
-  const navigation = useNavigation()
-  const isSubmitting = navigation.state === 'submitting'
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
@@ -48,13 +47,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input"/>
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input"/>
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,7 +61,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type="text" name="address" required className="input" />
           </div>
         </div>
 
@@ -73,42 +72,49 @@ function CreateOrder() {
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
-          <input type = 'hidden' name="cart" value={JSON.stringify(cart)}/>
-          <button disabled = {isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now' }</button>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing order..." : "Order now"}
+          </button>
         </div>
       </Form>
     </div>
   );
 }
 
-export async function action({request}){
-const formData = await request.formData()
-const data = Object.fromEntries(formData)
-console.log(data)
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
 
-const order = {
-  ...data,
-  cart: JSON.parse(data.cart),
-  priority: data.priority === 'on'
-}
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === "on",
+  };
 
-// to throw error message
-const errors = {}
+  // to throw error message
+  const errors = {};
 
-if(!isValidPhone(order.phone)) errors.phone = 'please give us your correct phone number we might need it to contact you'
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      "please give us your correct phone number we might need it to contact you";
 
-if(Object.keys(errors).length > 0) return errors;
+  if (Object.keys(errors).length > 0) return errors;
 
-// if everything is okay create new
-const newOrder =  await createOrder(order)
+  // if everything is okay create new
+  const newOrder = await createOrder(order);
 
-
-return redirect(`/order/${newOrder.id}`)
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
--- fast-react-pizza/src/features/order/SearchOrder.jsx
+++ fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function SearchOrder() {
   const [query, setQuery] = useState("");
-  const navigate = useNavigate()
-  function handleSubmit(e){
-    e.preventDefault()
-    if(!query) return;
-    navigate(`/order/${query}`)
-    setQuery('')
+  const navigate = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -17,6 +17,7 @@ export default function SearchOrder() {
         placeholder="Search Order"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="focus: w-28 rounded-full px-4 py-2 text-sm ring transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
