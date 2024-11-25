1f2fcfe31d3ed183aab99801dc5aa6d16842a024
charukirti <charukirticc112002@gmail.com>
2024-11-25 12:46:36 +0530

    implemented redux toolkit to implement create user funtionality

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant/";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,7 @@ function CreateOrder() {
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const userName = useSelector((state) => state.user.userName);
 
   return (
     <div className="px-4 py-6">
@@ -48,7 +50,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={userName}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
@@ -56,7 +64,9 @@ function CreateOrder() {
           <div className="grow">
             <input type="tel" name="phone" required className="input w-full" />
             {formErrors?.phone && (
-              <p className="mt-2 text-xs text-red-700 bg-red-100 rounded-md p-2 text-center">{formErrors.phone}</p>
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-center text-xs text-red-700">
+                {formErrors.phone}
+              </p>
             )}
           </div>
         </div>
--- fast-react-pizza/src/features/user/UserName.jsx
+++ fast-react-pizza/src/features/user/UserName.jsx
@@ -1,3 +1,9 @@
+import { useSelector } from "react-redux";
+
 export default function UserName() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const userName = useSelector((state) => state.user.userName);
+  if (!userName) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{userName}</div>
+  );
 }
