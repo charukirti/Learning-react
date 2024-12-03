2293cfdf5c7badd17d3115dfad908eaa361a7166
charukirti <charukirticc112002@gmail.com>
2024-12-03 20:44:26 +0530

    learned about styled component props amd css function

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -4,12 +4,6 @@ import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
 
-
-
-
-
-
-
 const StyledApp = styled.div`
   background-color: orangered;
   padding: 20px;
@@ -20,10 +14,13 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading>Hello World</Heading>
+        <Heading as="h1">Hello World</Heading>
+
+        <Heading as="h2">Chck in & out</Heading>
         <Button>Check In</Button>
         <Button>Check Out</Button>
 
+        <Heading as="h3">Form</Heading>
         <Input type="number" placeholder="number of guests" />
       </StyledApp>
     </>
