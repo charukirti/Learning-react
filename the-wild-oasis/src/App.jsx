d287c69a608e408cf9ee4447087f3cac3ea0a2f2
charukirti <charukirticc112002@gmail.com>
2024-12-03 20:22:49 +0530

    Learnt about global styles in styled components

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,8 +1,31 @@
-export default function App (){
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
+
+
+
+
+
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading>Hello World</Heading>
+        <Button>Check In</Button>
+        <Button>Check Out</Button>
+
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
+}
