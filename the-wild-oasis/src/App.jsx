5503eda31855e3d3595bc779d5b5b4b2a311a20e
charukirti <charukirticc112002@gmail.com>
2024-12-04 12:07:12 +0530

    Created AppLayout and sidebar and main navigation

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -11,20 +11,28 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import { Navigate } from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
---
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -0,0 +1,32 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+// making layout using grid
+
+const StyledAppLayout = styled.div`
+    display: grid;
+    grid-template-columns: 26rem 1fr;
+    grid-template-rows: auto 1fr;
+    height: 100vh;
+
+`
+
+const Main = styled.main`
+  background-color: green;
+  padding: 4rem 4.8rem 6.6rem;
+  background-color: var(--color-grey-50);
+`;
+
+export default function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
