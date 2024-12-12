fdb3366e98d01e8be7d9cc4333f6033636c7c5a6
charukirti <charukirticc112002@gmail.com>
2024-12-12 09:59:10 +0530

    did react-query setup

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "./styles/GlobalStyles";
 
@@ -13,9 +15,20 @@ import PageNotFound from "./pages/PageNotFound";
 import { Navigate } from "react-router-dom";
 import AppLayout from "./ui/AppLayout";
 
+// setting up react-query
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, //amount of time data store in cached
+    },
+  },
+});
+
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen='false'/>
       <GlobalStyles />
       <BrowserRouter
         future={{
@@ -37,6 +50,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
