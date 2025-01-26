874f663f08629d83db8f32954b860d77f9746b96
charukirti <charukirticc112002@gmail.com>
2025-01-26 18:21:55 +0530

    refactor cabin form: extract FormRow component and enhance validation

--- the-wild-oasis/src/ui/AppLayout.jsx
+++ the-wild-oasis/src/ui/AppLayout.jsx
@@ -16,6 +16,7 @@ const Main = styled.main`
   background-color: green;
   padding: 4rem 4.8rem 6.6rem;
   background-color: var(--color-grey-50);
+  overflow: scroll;
 `;
 
 const Container = styled.div`
