cf3c6c8d36728b536ced5cace8617fc285c799d1
charukirti <charukirticc112002@gmail.com>
2025-02-02 19:36:54 +0530

    learnt about composit component

--- 20-React-patterns/src/App.jsx
+++ 20-React-patterns/src/App.jsx
@@ -1,18 +1,24 @@
 import Counter from "./Counter";
 
-export default function App (){
+export default function App() {
   return (
     <div>
-    <h1>Compound Component Pattern</h1>
-    <Counter
-      iconIncrease="+"
-      iconDecrease="-"
-      label="My NOT so flexible counter"
-      hideLabel={false}
-      hideIncrease={false}
-      hideDecrease={false}
-    />
-  </div>
-  )
-}
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
 
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Decrease icon="-" />
+        <Counter.count />
+        <Counter.Increase icon="+" />
+      </Counter>
+    </div>
+  );
+}
--- 20-React-patterns/src/Counter.jsx
+++ 20-React-patterns/src/Counter.jsx
@@ -1,8 +1,44 @@
-export default function Counter (){
-    return (
-        <span>
-            Counter
-        </span>
-    )
+import { createContext, useContext, useState } from "react";
+
+// 1) create context
+const CounterContext = createContext();
+
+// 2) create Parent component
+export default function Counter({ children }) {
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount((e) => e + 1);
+  const decrease = () => setCount((e) => e - 1);
+  return (
+    <CounterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </CounterContext.Provider>
+  );
 }
 
+// 3) create child components that will help implement common functionality
+
+function Count() {
+  const { count } = useContext(CounterContext);
+  return <span>{count}</span>;
+}
+
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+
+function Increase({ icon }) {
+  const { increase } = useContext(CounterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+
+function Decrease({ icon }) {
+  const { decrease } = useContext(CounterContext);
+  return <button onClick={decrease}>{icon}</button>;
+}
+
+// 4. Add the child components to the parent component
+Counter.count = Count;
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
